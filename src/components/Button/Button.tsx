c00c91920bc72604bf733410f99de577fb118e5f
Dylan Peltier <peltdyla@gmail.com>
2024-08-26 00:45:03 -0400

    style: button styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,24 @@
 import React from "react"
+import "./Button.css"
 
-type Props = {
-  text: string
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary"
 }
 
-const Button: React.FC<Props> = ({ text }) => {
-  return <button>{text}</button>
+const Button: React.FC<ButtonProps> = ({ variant, ...props }) => {
+  const colorClasses = () => {
+    switch (variant) {
+      case "primary":
+        return { borderColor: '#6366F1', backGroundColor: '#6366F1', color: '#FAFAFA' }
+      case "secondary":
+        return { borderColor: '#6366F1', backGroundColor: '#FAFAFA', color: '#6366F1' }
+      default:
+        return { borderColor: '#6366F1', backGroundColor: '#6366F1', color: '#FAFAFA' }
+    }
+  }
+  return <button style={{ ...colorClasses(), ...props.style }} className="px-4 py-2 rounded-md" {...props}>
+    {props.children}
+  </button>
 }
 
 export default Button
