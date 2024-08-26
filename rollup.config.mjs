d3cfcb1822ebc0740eb8bc50d65cb85ef7bbd16f
Dylan Peltier <peltdyla@gmail.com>
2024-08-26 00:08:06 -0400

    fix: added npm publishing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,8 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import plugin from "rollup-plugin-dts";
 import dts from "rollup-plugin-dts";
-
-const packageJson = require("./package.json");
+import packageJson from "./package.json" with { type: "json" };
 
 export default [
   {
